d2bad3a19361f18a8d8595bb5fe82ebf53bc2020
Lars Vierbergen <vierbergenlars@gmail.com>
2016-06-23 23:11:23 +0200

    Allow to skip setting application variable when it already exists (either local or global)

--- src/Command/Application/VariableSetCommand.php
+++ src/Command/Application/VariableSetCommand.php
@@ -9,6 +9,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\Question;
+use vierbergenlars\CliCentral\Exception\Configuration\MissingConfigurationParameterException;
 use vierbergenlars\CliCentral\Helper\GlobalConfigurationHelper;
 
 class VariableSetCommand extends Command
@@ -21,6 +22,8 @@ class VariableSetCommand extends Command
             ->addArgument('value', InputArgument::REQUIRED)
             ->addOption('description', null, InputOption::VALUE_REQUIRED)
             ->addOption('default', null, InputOption::VALUE_REQUIRED, 'Default value')
+            ->addOption('if-not-exists', null, InputOption::VALUE_NONE, 'Do not set the variable if it already is set')
+            ->addOption('if-not-global-exists', null, InputOption::VALUE_NONE, 'Do not set the variable if a global value is already set')
             ->setDescription('Sets variable value for an application')
             ->setHelp(<<<'EOF'
 The <info>%command.name%</info> command sets the value of a variable for an application:
@@ -29,6 +32,9 @@ The <info>%command.name%</info> command sets the value of a variable for an appl
 
 All variables contain plain text, arrays or objects are not permitted.
 Variables are stored in the overrides definition for the application.
+
+To only set the variable if it does not exist yet, use the <comment>--if-not-exists</comment> option.
+To only set the variable if the global variable does not exist yet, use the <comment>--if-not-global-exists</comment> option.
 
 If the <comment>value</comment> argument is not filled and the command is run in interactive mode,
 a value for the variable will be asked interactively to the user.
@@ -45,8 +51,37 @@ EOF
         ;
     }
 
+    private function checkIfNotExists(InputInterface $input)
+    {
+        if(!$input->getOption('if-not-exists'))
+            return false;
+        $configurationHelper = $this->getHelper('configuration');
+        /* @var $configurationHelper GlobalConfigurationHelper */
+        $application = $configurationHelper->getConfiguration()->getApplication($input->getArgument('application'));
+        $overrides = $application->getOverrides();
+        if(!isset($overrides->vars))
+            return false;
+        return isset($overrides->vars->{$input->getArgument('variable')});
+    }
+
+    private function checkIfNotGlobalExists(InputInterface $input)
+    {
+        if(!$input->getOption('if-not-global-exists'))
+            return false;
+        $configurationHelper = $this->getHelper('configuration');
+        /* @var $configurationHelper GlobalConfigurationHelper */
+        try {
+            $configurationHelper->getConfiguration()->getGlobalVariable($input->getArgument('variable'));
+            return true;
+        } catch(MissingConfigurationParameterException $ex) {
+            return false;
+        }
+    }
+
     protected function interact(InputInterface $input, OutputInterface $output)
     {
+        if($this->checkIfNotExists($input)||$this->checkIfNotGlobalExists($input))
+            return;
         if(!$input->getArgument('application')||!$input->getArgument('variable'))
             return;
         if(!$input->getOption('description')) {
@@ -61,9 +96,10 @@ EOF
         }
     }
 
-
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        if($this->checkIfNotExists($input)||$this->checkIfNotGlobalExists($input))
+            return;
         $configurationHelper = $this->getHelper('configuration');
         /* @var $configurationHelper GlobalConfigurationHelper */
         $application = $configurationHelper->getConfiguration()->getApplication($input->getArgument('application'));
